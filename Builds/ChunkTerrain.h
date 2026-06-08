bfe4b508954899e85f0586ae43e06054b0c08dea
xbz-24 <renatochavez7@gmail.com>
2026-06-08 04:45:46 -0300

    Refine voxel engine rendering pipeline

---
+++ Builds/ChunkTerrain.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Block.h"
+
+namespace ve::world::terrain
+{
+	constexpr int ChunkWidth = 16;
+	constexpr int ChunkHeight = 128;
+	constexpr int ChunkDepth = 16;
+	using BlockStorage = ve::blocks::BlockId[ChunkWidth][ChunkHeight][ChunkDepth];
+
+	/**
+	 * Fills chunk block storage with procedural terrain.
+	 *
+	 * @param chunkX Chunk-grid X coordinate.
+	 * @param chunkZ Chunk-grid Z coordinate.
+	 * @param blocks Storage filled with generated block ids.
+	 */
+	void GenerateChunkTerrain(int chunkX, int chunkZ, BlockStorage& blocks);
+}
+
