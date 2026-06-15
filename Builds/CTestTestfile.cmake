9299033170ebd4a83b80d709ff5a30e605c0bd98
xbz-24 <renatochavez7@gmail.com>
2026-06-15 14:26:10 -0300

    Add CMake presets and inline mesh cube builder

---
+++ Builds/CTestTestfile.cmake
@@ -0,0 +1,21 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/renat/source/repos/voxel_engine
+# Build directory: C:/Users/renat/source/repos/voxel_engine/Builds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+if(CTEST_CONFIGURATION_TYPE MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+  add_test([=[engine_tests]=] "C:/Users/renat/source/repos/voxel_engine/Builds/Debug/engine_tests.exe")
+  set_tests_properties([=[engine_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/renat/source/repos/voxel_engine/CMakeLists.txt;321;add_test;C:/Users/renat/source/repos/voxel_engine/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+  add_test([=[engine_tests]=] "C:/Users/renat/source/repos/voxel_engine/Builds/Release/engine_tests.exe")
+  set_tests_properties([=[engine_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/renat/source/repos/voxel_engine/CMakeLists.txt;321;add_test;C:/Users/renat/source/repos/voxel_engine/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+  add_test([=[engine_tests]=] "C:/Users/renat/source/repos/voxel_engine/Builds/MinSizeRel/engine_tests.exe")
+  set_tests_properties([=[engine_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/renat/source/repos/voxel_engine/CMakeLists.txt;321;add_test;C:/Users/renat/source/repos/voxel_engine/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+  add_test([=[engine_tests]=] "C:/Users/renat/source/repos/voxel_engine/Builds/RelWithDebInfo/engine_tests.exe")
+  set_tests_properties([=[engine_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/renat/source/repos/voxel_engine/CMakeLists.txt;321;add_test;C:/Users/renat/source/repos/voxel_engine/CMakeLists.txt;0;")
+else()
+  add_test([=[engine_tests]=] NOT_AVAILABLE)
+endif()
--- Builds/MeshBuilder.h
+++ Builds/MeshBuilder.h
@@ -60,24 +60,7 @@ namespace ve::rendering
 		}
 
 		/** @param center Cube center. @param size Cube size by axis. @param color Vertex color. @return This builder. */
-		BasicMeshBuilder& AddCube(glm::vec3 center, glm::vec3 size, ColorRgba color = {})
-		{
-			const glm::vec3 half_size = size * 0.5f;
-			const glm::vec3 minimum = center - half_size;
-			const glm::vec3 maximum = center + half_size;
-			const std::array<glm::vec3, 8> corners{ {
-				{ minimum.x, minimum.y, minimum.z }, { maximum.x, minimum.y, minimum.z },
-				{ maximum.x, maximum.y, minimum.z }, { minimum.x, maximum.y, minimum.z },
-				{ minimum.x, minimum.y, maximum.z }, { maximum.x, minimum.y, maximum.z },
-				{ maximum.x, maximum.y, maximum.z }, { minimum.x, maximum.y, maximum.z } } };
-			AddQuad(corners[0], corners[1], corners[2], corners[3], color);
-			AddQuad(corners[4], corners[7], corners[6], corners[5], color);
-			AddQuad(corners[0], corners[4], corners[5], corners[1], color);
-			AddQuad(corners[1], corners[5], corners[6], corners[2], color);
-			AddQuad(corners[2], corners[6], corners[7], corners[3], color);
-			AddQuad(corners[3], corners[7], corners[4], corners[0], color);
-			return *this;
-		}
+		BasicMeshBuilder& AddCube(glm::vec3 center, glm::vec3 size, ColorRgba color = {});
 
 		/** @return Mesh data moved out of the builder. */
 		[[nodiscard]] Mesh Build() && noexcept { return std::move(mesh_); }
@@ -110,3 +93,5 @@ namespace ve::rendering
 
 	using MeshBuilder = BasicMeshBuilder<>;
 }
+
+#include "MeshBuilderCube.inl"
