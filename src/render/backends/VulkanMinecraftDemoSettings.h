61a3f6c7e90094667130a24567c87a88708eb1fb
xbz-24 <renatochavez7@gmail.com>
2026-07-07 06:37:43 -0300

    Remove unused Minecraft demo profile API

--- src/render/backends/VulkanMinecraftDemoSettings.h
+++ src/render/backends/VulkanMinecraftDemoSettings.h
@@ -1,10 +1,6 @@
 #pragma once
 
 #include <cstdint>
-#include <cstddef>
-#include <string_view>
-
-#include <glm/vec3.hpp>
 
 namespace ve::rendering
 {
@@ -49,22 +45,6 @@ namespace ve::rendering
 		[[nodiscard]] bool operator==(const VulkanMinecraftDemoSceneConfig&) const = default;
 	};
 
-	struct VulkanMinecraftDemoProfile
-	{
-		VulkanMinecraftDemoPreset preset = VulkanMinecraftDemoPreset::HyperrealDesert;
-		std::string_view display_name;
-		std::string_view window_title;
-		int world_size_chunks = 20;
-		int render_distance_chunks = 16;
-		glm::vec3 camera_position{ 154.0f, 82.0f, 292.0f };
-		glm::vec3 camera_target{ 160.0f, 52.0f, 86.0f };
-		VulkanMinecraftDemoSceneConfig scene{};
-		std::string_view model_asset_keyword;
-		int model_stage_radius = 0;
-		int model_target_extent = 0;
-		std::size_t model_voxel_budget = 0;
-	};
-
 	struct VulkanMinecraftDemoSettings
 	{
 		VulkanMinecraftDemoSceneConfig scene{};
@@ -91,6 +71,4 @@ namespace ve::rendering
 	};
 
 	[[nodiscard]] const char* VulkanMinecraftDemoPresetName(VulkanMinecraftDemoPreset preset) noexcept;
-	[[nodiscard]] VulkanMinecraftDemoProfile VulkanMinecraftDemoProfileFor(VulkanMinecraftDemoPreset preset) noexcept;
-	[[nodiscard]] VulkanMinecraftDemoSceneConfig VulkanMinecraftDemoDefaultSceneConfig(VulkanMinecraftDemoPreset preset) noexcept;
 }
