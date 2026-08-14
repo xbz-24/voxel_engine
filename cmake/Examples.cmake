80c90a8bce5c80477f646027ff8c263ada5d8149
xbz-24 <renatochavez7@gmail.com>
2026-08-14 02:37:59 -0300

    Exercise deterministic SDK examples

--- cmake/Examples.cmake
+++ cmake/Examples.cmake
@@ -17,4 +17,31 @@ if (VE_BUILD_EXAMPLES)
     ve_add_public_example(voxel_example_scene_graph "${PROJECT_SOURCE_DIR}/examples/scene_graph.cpp")
     ve_add_public_example(voxel_example_runtime_commands "${PROJECT_SOURCE_DIR}/examples/runtime_commands.cpp")
     ve_add_public_example(voxel_example_runtime_callbacks "${PROJECT_SOURCE_DIR}/examples/runtime_callbacks.cpp")
+
+    set(VE_PUBLIC_EXAMPLE_TARGETS
+        voxel_example_minimal
+        voxel_example_custom_scene
+        voxel_example_world_edit
+        voxel_example_asset_catalog
+        voxel_example_scene_graph
+        voxel_example_runtime_commands
+        voxel_example_runtime_callbacks
+    )
+    add_custom_target(voxel_examples)
+    add_dependencies(voxel_examples ${VE_PUBLIC_EXAMPLE_TARGETS})
+    set_target_properties(voxel_examples PROPERTIES FOLDER "Examples")
+
+    if (VE_BUILD_TESTS)
+        enable_testing()
+        foreach(headless_example IN ITEMS
+                voxel_example_asset_catalog
+                voxel_example_scene_graph
+                voxel_example_runtime_commands)
+            add_test(NAME ${headless_example} COMMAND $<TARGET_FILE:${headless_example}>)
+            set_tests_properties(${headless_example} PROPERTIES
+                LABELS "api;examples;headless"
+                TIMEOUT 30
+            )
+        endforeach()
+    endif()
 endif()
